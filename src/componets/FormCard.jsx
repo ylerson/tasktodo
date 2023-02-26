cc18de5e50762af69f645262a0479b3f292c2322
Ylerson Almonte <yacwarefree1@gmail.com>
2023-02-26 15:06:50 +0000

    Update App.css and FormCard.jsx

--- src/componets/FormCard.jsx
+++ src/componets/FormCard.jsx
@@ -3,6 +3,7 @@ export default function Form(props) {
     <form onSubmit={props.handleAddNewTask} className="input_header">
       <input
         name="title"
+        autoComplete="false"
         placeholder="What is your next task?"
         className="input_add_task"
       />
